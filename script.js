3a3c086f6589a146b6fb0e184cac95b97db5a502
J.M. Bock <j.m.bock@JMs-MacBook-Air.local>
2021-04-13 03:26:11 -0500

    first 2 requirements completed

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 // Write your JavaScript code here!
+   
+window.addEventListener("load", function() {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+     let pilotNameInput = form.elements['pilotName'].value;
+     let copilotName = document.querySelector("input[name=copilotName]");
+     let fuelLevelInput = form.elements['fuelLevel'].value;
+     let cargoMassInput = form.elements['cargoMass'].value;
+     if (pilotNameInput === "" || copilotName.value === "" || fuelLevelInput === "" || cargoMassInput === "") {
+         alert("All fields are required!");
+     //  stop the form submission
+     event.preventDefault();
+      };
+   });
+ });
+
+
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
